e90fd193a8fb264609d9ba7eefbe5d98c90cea71
Fernanda-15 <fernandaqp15@gmail.com>
2023-10-22 14:44:03 -0600

    url Allmovies

--- src/components/movieIndex.vue
+++ src/components/movieIndex.vue
@@ -45,7 +45,7 @@ export default {
     methods: {
         allMovies() {
             console.log("URL", this.url);
-            fetch(this.url + '/netlify/functions/movieFindAll',
+            fetch(this.url + '/.netlify/functions/movieFindAll',
                 { headers: { 'Accept': 'application/json' } })
                 .then((response) => response.json())
                 .then((items) => {
